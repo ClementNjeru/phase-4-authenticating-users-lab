1a4c27b246cfcaf572c78a9864a86373b521c883
ClementNjeru <njeruclement36@gmail.com>
2022-10-04 07:11:44 +0300

    commit tree 979293c82f3d33cb25b81ca865c65c998a1a5c2c

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+  #  This controller has one action, create, which we'll map in routes.rb for POST requests to /login:
+  post "/login", to: "sessions#create"
+
+  # The log out flow is even simpler. We can add a new route for logging out:
+  delete "/logout", to: "sessions#destroy"
+
+  # we need a route to retrieve the user's data from the database using the session hash:
+  get "/me", to: "users#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
